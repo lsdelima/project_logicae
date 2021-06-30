0bb6a078c2424f57992ba0c124850645a6fbd052
Luís de Lina <luis.ricardo.stl@gmail.com>
2021-06-30 14:40:05 -0300

    Logicae 1.0.0r

--- game/code/scr/user_interface/_logo.gd
+++ game/code/scr/user_interface/_logo.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_Timer_timeout():
-	get_tree().change_scene("res://scr/user_interface/level_lataria.tscn")
+	get_tree().change_scene("res://scr/user_interface/level_ifba.tscn")
